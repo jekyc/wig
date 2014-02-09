6c250ec471c7dffc69f631a8b4b114c31e49f4c5
jekyc <jesper@kuckelhahn.dk>
2014-02-09 17:08:11 +0100

    added demandware fingerprints

---
+++ data/cms/string/demandware.py
@@ -0,0 +1,18 @@
+[
+	{
+		"url": "/",
+		"string": "Demandware Analytics code 1.0",
+		"output": ""
+	},
+	{
+		"url": "/",
+		"string": "demandware.static",
+		"output": ""
+	},
+	{
+		"url": "/",
+		"string": "on/demandware.store/",
+		"output": ""
+	}	
+]
+
