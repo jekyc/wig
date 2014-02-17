7518fc185cbefcba54e76d32b6c3574830616225
jekyc <jesper@kuckelhahn.dk>
2014-02-18 00:33:41 +0100

    fixed issue with adding boolean results to results

--- classes/results.py
+++ classes/results.py
@@ -12,6 +12,8 @@ class Results(object):
 		v = data['version']
 		dw = data['count']
 
+		if type(v) == bool: v = ""
+
 		if type(weight) == bool:
 			w = 1.0/dw if weight else dw
 			self.results[category][plugin][v] += w
