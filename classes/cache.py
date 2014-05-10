66144f701953820ac6845da87049b85f4c4eb3e6
jekyc <jesper@kuckelhahn.dk>
2014-05-11 00:31:29 +0200

    Added host property to cache

--- classes/cache.py
+++ classes/cache.py
@@ -3,6 +3,7 @@ import queue
 class Cache(queue.Queue):
 	def _init(self, maxsize):
 		self.queue = dict()
+		self.host = None
 
 	def __getitem__(self, path):
 		return self.queue[path]
@@ -14,6 +15,9 @@ class Cache(queue.Queue):
 	def __contains__(self, item):
 		with self.mutex:
 			return item in self.queue
+
+	def set_host(self, host):
+		self.host = host
 
 	def get_num_urls(self):
 		return len(self.queue.keys())
