8b6cc129f91d64f23190a35d9df039deaed31924
jekyc <jesper@kuckelhahn.dk>
2014-06-29 16:01:12 +0200

    Added string matching

--- plugins/roundcube.py
+++ plugins/roundcube.py
@@ -1,11 +1,21 @@
-from classes.specializedRequesters import CMSReqMD5
+from classes.specializedRequesters import CMSReqMD5, CMSReqString
 
-class RoundCube(CMSReqMD5):
+class RoundCubeString(CMSReqString):
 	def __init__(self, host, cache, results):
 		super().__init__(host, cache, results)
 		self.name	= "RoundCube"
-		self.prefix	= ["/webmail", "/mail", "/roundcube"]
+		self.prefix	= ["", "/webmail", "/mail", "/roundcube"]
+		self.data_file	= "data/cms/string/roundcube.json"
+
+class RoundCubeMD5(CMSReqMD5):
+	def __init__(self, host, cache, results):
+		super().__init__(host, cache, results)
+		self.name	= "RoundCube"
+		self.prefix	= ["/webmail", "/mail", "/roundcube", ""]
 		self.data_file	= "data/cms/md5/roundcube.json"
 
 def get_instances(host, cache, results):
-	return [RoundCube(host, cache, results)]
+	return [
+		RoundCubeMD5(host, cache, results),
+		RoundCubeString(host, cache, results)
+	]
