6e9e05636223373ec1ca066347fa4c357ed570a9
jekyc <jesper@kuckelhahn.dk>
2014-11-13 01:15:08 +0100

    Adjusted/aligned output

--- classes/output.py
+++ classes/output.py
@@ -2,10 +2,18 @@ from classes.color import Color
 
 class Output(object):
 
-	def __init__(self, results):
+	def __init__(self, results, runtime, url_count, fp_count):
 		self.results = results
 		self.color = Color()
-		self.col_widths =  { 1: 0, 2: 0, 3: 0, 0: 0}
+
+		self.stats = {
+			'runtime':		'Time: %.1f sec' % (runtime, ),
+			'url_count':	'Urls: %s' % (url_count, ),
+			'fp_count':		'Fingerprints: %s' % (fp_count, ),
+		}
+
+		self.col_widths =  {1: 0, 2: 0, 3: 0}
+
 
 		self.sections = [
 			{
@@ -38,16 +46,11 @@ class Output(object):
 		self.seperator = ' | '
 		self.seperator_color = self.color.format(self.seperator, 'blue', True)
 
-		self.stats = {
-			'runtime': None,
-			'number_of_urls': None,
-			'number_of_fps': None,
-		}
-
-
 		self._set_col_1_width(results)
 		self._set_col_2_width(results)
 		self._set_col_3_width(results)
+
+
 
 
 	def _find_section_index(self, section):
@@ -58,53 +61,57 @@ class Output(object):
 
 		return None
 
-	def _update_col_0(self):
-		self.col_widths[0] = self.col_widths[1] + self.col_widths[2] + self.col_widths[3]
-
 
 	def _set_col_1_width(self, results):
-		title_len = max([len(i['headers'][1]['title']) for i in self.sections])
-		self.col_widths[1] = max(max([len(p) for c in results for p in results[c]]), title_len) + 2
-		self._update_col_0()
+		self.col_widths[1] = 2 + max(
+			max([len(i['headers'][1]['title']) for i in self.sections]),	# length of section header titles
+			max([len(p) for c in results for p in results[c]]), 			# length of software name from results
+			len(self.stats['runtime'])										# length of status bar (time)
+		)
 
 
-	def _set_col_2_width(self, results):
-		width = max([ len(i['headers'][2]['title']) for i in self.sections])
-		for c in results:
-			for p in results[c]:
-				width = max(len(self.seperator.join(results[c][p])), width)
-
-		self.col_widths[2] = width + 2
-		self._update_col_0()
+	def _set_col_2_width(self, results):		
+		self.col_widths[2] = 2 + max(
+			max([ len(i['headers'][2]['title']) for i in self.sections ]),							# length of section header titles
+			max([ len(self.seperator.join(results[c][p])) for c in results for p in results[c] ]),	# length of version details from results
+			len(self.stats['url_count'])															# length of status bar (urls)
+		)
 		
 
 	def _set_col_3_width(self, results):
-		self.col_widths[3] = max([len(i['title']) for s in self.sections for i in s['titles']]) 
-		self._update_col_0()
-
-
-	def set_number_urls(self, num_urls): self.stats['number_of_urls'] = num_urls
-	def set_runtime(self, runtime):      self.stats['runtime'] = "%.1f" % (runtime,)
-	def set_number_fps(self, num_fps):   self.stats['number_of_fps'] = num_fps
+		self.col_widths[3] = max(
+			max([len(i['title']) for s in self.sections for i in s['titles']]),	# length of titles
+			len(self.stats['fp_count'])												# length of status bar (fps)
+		)
 
 
 	def create_header(self, section):
 		section_index = self._find_section_index(section)
 		headers = self.sections[section_index]['headers']
-		
 		out  = ''
-		for i in range(1,4):
-			out += self.color.format(headers[i]['title'], headers[i]['color'], headers[i]['bold']) 
-			out += ' ' * (self.col_widths[i] - len(headers[i]['title']))
+
+		# col 1
+		out += self.color.format(headers[1]['title'], headers[1]['color'], headers[1]['bold'])
+		out += ' ' * (self.col_widths[1] - len(headers[1]['title']))
+
+		# col 2
+		out += self.color.format(headers[2]['title'], headers[2]['color'], headers[2]['bold'])
+		out += ' ' * (self.col_widths[2] - len(headers[2]['title']))
+
+		# col 3
+		out += self.color.format(headers[3]['title'], headers[3]['color'], headers[3]['bold'])
 		out += '\n'
 
 		return out
 
 
 	def create_line(self, software, versions, category):
+
+		# col 1
 		out  = software
 		out += ' ' * (self.col_widths[1] - len(software))
 		
+		#col 2
 		if len(versions) > 1:
 			out += self.seperator_color.join(versions)
 			out += ' ' * (self.col_widths[2] - len(self.seperator.join(versions)))			
@@ -112,39 +119,15 @@ class Output(object):
 			out += versions[0]
 			out += ' ' * (self.col_widths[2] - len(versions[0]))
 
+		# col3
 		out += category 
-		out += ' ' * (self.col_widths[3] - len(category))
 		out += '\n'
 
 		return out
 
-
-	def get_status(self):
-		time = 'Time: %s sec' % (self.stats['runtime'], )
-		urls = 'Urls: %s' % (self.stats['number_of_urls'], )
-		fps  = 'Fingerprints: %s' % (self.stats['number_of_fps'], )
-
-		# if the current width of the output is smaller that the 
-		# width of the status bar, extend the width of column 2
-		min_width = len('  '.join([ time, urls, fps ]))
-
-		if min_width > self.col_widths[0]:
-			self.col_widths[2] = min_width - (self.col_widths[1] + self.col_widths[3])
-			self._update_col_0()
-		else:
-			time = time + ' ' * (self.col_widths[1] - len(time) - 2 )
-			urls = urls + ' ' * (self.col_widths[2] - len(urls) - 5 ) 
-			fps  = ' ' * (self.col_widths[3] - len(fps)) + fps
-
-		# generate the status bar
-		status  = self.color.format('_'*self.col_widths[0], 'blue', True) + '\n'
-		status += '  '.join([ time, urls, fps ])
-		return status
-
 	
 	def get_results(self):
-		status_bar = self.get_status()
-
+		
 		out = ''
 		for section in ['version', 'interesting']:
 
@@ -160,4 +143,13 @@ class Output(object):
 				out += self.create_header(section)
 				out += tmp
 
-		return out + status_bar + '\n'
+
+		# status bar
+		time = self.stats['runtime']   + ' ' * (self.col_widths[1] - len(self.stats['runtime']))
+		urls = self.stats['url_count'] + ' ' * (self.col_widths[2] - len(self.stats['url_count'])) 
+		fps  = self.stats['fp_count']
+
+		status  = self.color.format('_'*sum(self.col_widths.values()), 'blue', True) + '\n'
+		status += ''.join([ time, urls, fps ])
+
+		return out + status + '\n'
