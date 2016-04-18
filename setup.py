b7025b3fd327243dd75271a9fcfd6f5b93fc8b4b
Fabian Martinez Portantier <fportantier@securetia.com>
2016-04-18 13:55:58 -0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='wig',
+      version='1.0',
+      description='WebApp Information Gatherer',
+      author='jekyc',
+      #author_email='none@none.com',
+      url='https://github.com/jekyc/wig',
+      packages=['wig'],
+     )
+
